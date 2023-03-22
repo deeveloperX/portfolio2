d5c83359658c85ba4916ac06862816be04f2604c
deeveloperX <120943438+deeveloperX@users.noreply.github.com>
2023-03-22 21:17:25 +0500

    change in code for footer

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ const navbarNav = document.querySelector('.navbar-nav');
 navbarToggle.addEventListener('click', () => {
   navbarNav.classList.toggle('show');
 });
+
+const submit = document.getElementById("button");
+const naam = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+function messageAction(){
+  if(message != "" && email != "" && naam != ""){
+    alert("Sorry! this section is still in test mode.")
+  }
+}
